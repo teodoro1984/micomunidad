69fcf114dfc473d001c8e73cfb44125456a5b010
teodoro1984 <teohervel@gmail.com>
2025-04-20 21:08:01 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,98 @@
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<link rel="stylesheet" href="css/style.css?02082023">
+	<link rel="stylesheet" href="css/normalize.css">
+	<link rel="canonical" href="https://teohervelxmen.webcindario.com/index.php"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <meta name="keywords" content="san isidro, las banderas, pantepec, chciapas,">
+    <meta name="description" content="Aqui te presentamos datos o informaciones sobre la localidad de San Isidro las Banderas del municipio de Pantepec Chiapas.">
+    <title>Pagina principal</title>
+    
+
+    <script class="u-script" type="text/javascript" src="jquery.js" defer=""></script>
+    
+    
+    <link rel="icon" href="img/favicon.ico">
+    <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i">
+    <link id="u-page-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lobster:400">
+    
+    
+    <script type="application/ld+json">{
+		"@context": "http://schema.org",
+		"@type": "Organization",
+		"name": "MI ESPACIO VIRTUAL",
+		"logo": "img/Bunch-of-Balloons-PNG-Photos.png",
+		"sameAs": [
+				"https://facebook.com/name",
+				"https://twitter.com/name",
+				"https://instagram.com/name"
+		]
+}</script>
+    <meta name="theme-color" content="#478ac9">
+    <meta name="twitter:site" content="@">
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:title" content="CONTACTANOS">
+    <meta name="twitter:description" content="MI ESPACIO VIRTUAL">
+	<!-- Aqui se empiza a definir como se mostrara en facebook al compartir la publicacion -->
+    <meta property="og:title" content="SAN ISIDRO LAS BANDERAS.">
+    <meta property="og:description" content="Conoce la localidad y sus costumbres.">
+    <meta property="og:type" content="website">
+	<meta property="og:image" name="facebook:image" content="https://teohervelxmen.webcindario.com/img/imagen de inicio.jpg" />
+  <meta data-intl-tel-input-cdn-path="intlTelInput/">
+</head>
+
+<div id = "fb-root" > </div> <script async defer crossorigin = "anonymous" src = "https://connect.facebook.net/es_ES/sdk.js#xfbml=1&version=v3.3&appId= 2158694887700796 & autoLogAppEvents = 1 " > </script>
+
+
+<div class="contenedor">
+
+<?php include("menu de navegacion.php"); ?>
+
+
+<div class="caja-principal">
+
+                   <div class="cuerpo">
+                  <div class="titulodelapublicacion"> 
+				  <div><h1>Escribe el titulo del contenido.</h1></div>
+				   <div class="compartir"><a href="https://www.facebook.com/sharer.php?u=https://teohervelxmen.webcindario.com" target="_blank"><img alt="Sígueme en Facebook" height="80px" width="80px" src="img/compartir.gif" title="Sígueme en Facebook"></a></div>
+                   </div>
+                                 <!-- En este apartado escribe tu contenido principal -->
+                                  
+                   			   
+                   			   <p>Si tienes dudas dejanos tus comentarios y en cuanto podamos con gusto responderemos tus inquietudes.</p>
+                   			   <!-- En index.php cambiar por el nombre de la pagina -->
+                   
+                               <div class = "fb-comments" data-href = "https://teohervelxmen.webcindario.com/index.php" data-width = "100%" data-numposts = "10" > </div>
+                   
+                   
+                   </div>
+				   
+<div class="barra-lateral">
+
+<?php include("entradas.php"); ?>
+
+
+</div>
+</div>
+<div class="pie">
+
+
+  <?php include("pie de pagina.php"); ?>
+
+</div>
+
+
+</div>
+
+<style>
+.compartir{width:100px;
+           height:100px;
+			margin-left:30px;
+			margin-bottom:0px;}
+			
+.titulodelapublicacion{width:100%;
+                       display:flex;}
+</style>
+
